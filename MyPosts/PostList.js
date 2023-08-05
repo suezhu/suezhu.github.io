bf49c2d7fd3a74ab24e23a01d16156bfff8118b7
suezhu <suezhu@yahoo.com>
2023-08-04 18:28:31 -0700

    chinese

--- MyPosts/PostList.js
+++ MyPosts/PostList.js
@@ -1,18 +1,22 @@
 var PostList = [
-    ["20230706/20230706.html", "July 6, 2023", "July 6, 2023 - Lassen National Park, CA"],
-    ["20230619.html", "June 19, 2023", "June 19, 2023 - Seattle Summer Release, WA"],
+    ["20230706/20230706.html", "July 6, 2023", "July 6, 2023 - Lassen National Park, CA", "20230706/20230706c.html"],
+    ["20230619.html", "June 19, 2023", "June 19, 2023 - Seattle Summer Release, WA", "20230619chtml"],
     ];
 
 var middleRightTextPostList = "";
 var middleRightTextPostListDetail = "";
 for(var i = 0; i < PostList.length; i++) {
     if (PostList[i][0] == PageName){
-        middleRightTextPostList += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][1] + "</a></b></div>";
-        middleRightTextPostListDetail += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][2] + "</a></b></div>";
+        middleRightTextPostList += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][1] + "</a>" + 
+                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> ?? </a></b></div>";
+        middleRightTextPostListDetail += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][2] + "</a>" +
+                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> ?? </a></b></div>";
     }
     else {
-        middleRightTextPostList += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][1] + "</a></div>";
-        middleRightTextPostListDetail += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][2] + "</a></div>";
+        middleRightTextPostList += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][1] + "</a>" +
+                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> ?? </a></div>";
+        middleRightTextPostListDetail += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][2] + "</a>" +
+                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> ?? </a></div>";
     }
 }
         
