4eb6a3d30d8d54c1a202728106db7f73bf135dce
suezhu <suezhu@yahoo.com>
2023-11-10 13:13:23 -0800

    add 2022 posts
    add 2022 posts

--- MyPosts/PostList.js
+++ MyPosts/PostList.js
@@ -1,32 +1,50 @@
+var FolderList = [
+    ["2023"],
+    ["2022"],
+     ];
+    
 var PostList = [
-    ["20230715/20230715.html", "July 15, 2023", "July 15, 2023 - Camping with GO PNW 2023 Summit, OR", "20230715/20230715c.html"],
-    ["20230706/20230706.html", "July 6, 2023", "July 6, 2023 - Camping at Lassen National Park, CA", "20230706/20230706c.html"],
-    ["20230619/20230619.html", "June 19, 2023", "June 19, 2023 - Seattle Summer Release, WA", "20230619/20230619c.html"],
-    ["20230530/20230530.html", "May 30, 2023", "May 30, 2023 - Camping at Bruneau Dunes State Park, ID", "20230530/20230530c.html"],
-    ["20230507/20230507.html", "May 7, 2023", "May 7, 2023 - Camping at Sol Duc Hot Spring, WA", "20230507/20230507c.html"],
-    ["20230423/20230423.html", "April 23, 2023", "April 23, 2023 - Camping at Le Page Park, OR", "20230423/20230423c.html"],
-    ["20230409/20230409.html", "April 9, 2023", "April 9, 2023 - Camping at Battle Ground Lake, WA", "20230409/20230409c.html"],
-    ["20230401/20230401.html", "April 1, 2023", "April 1, 2023 - April Fool Seattle, WA", "20230401/20230401c.html"],
-    ["20230224/20230224.html", "February 24, 2023", "February 24, 2023 - Skiing at Granby, CO", "20230224/20230224c.html"],
-    ["20230128/20230128.html", "January 28, 2023", "January 28, 2023 - Skiing at Stevens, WA", "20230128/20230128c.html"],
-    ["20230101/20230101.html", "January 1, 2023", "January 1, 2023 - Skiing at Lake Tahoe, NV", "20230101/20230101c.html"],
-    ];
+    ["2023/20230715/20230715.html", "July 15, 2023", "July 15, 2023 - Camping with GO PNW 2023 Summit, OR", "2023/20230715/20230715c.html", "2023"],
+    ["2023/20230706/20230706.html", "July 6, 2023", "July 6, 2023 - Camping at Lassen National Park, CA", "2023/20230706/20230706c.html", "2023"],
+    ["2023/20230619/20230619.html", "June 19, 2023", "June 19, 2023 - Seattle Summer Release, WA", "2023/20230619/20230619c.html", "2023"],
+    ["2023/20230530/20230530.html", "May 30, 2023", "May 30, 2023 - Camping at Bruneau Dunes State Park, ID", "2023/20230530/20230530c.html", "2023"],
+    ["2023/20230507/20230507.html", "May 7, 2023", "May 7, 2023 - Camping at Sol Duc Hot Spring, WA", "2023/20230507/20230507c.html", "2023"],
+    ["2023/20230423/20230423.html", "April 23, 2023", "April 23, 2023 - Camping at Le Page Park, OR", "2023/20230423/20230423c.html", "2023"],
+    ["2023/20230409/20230409.html", "April 9, 2023", "April 9, 2023 - Camping at Battle Ground Lake, WA", "2023/20230409/20230409c.html", "2023"],
+    ["2023/20230401/20230401.html", "April 1, 2023", "April 1, 2023 - April Fool Seattle, WA", "2023/20230401/20230401c.html", "2023"],
+    ["2023/20230224/20230224.html", "February 24, 2023", "February 24, 2023 - Skiing at Granby, CO", "2023/20230224/20230224c.html", "2023"],
+    ["2023/20230128/20230128.html", "January 28, 2023", "January 28, 2023 - Skiing at Stevens, WA", "2023/20230128/20230128c.html", "2023"],
+    ["2023/20230101/20230101.html", "January 1, 2023", "January 1, 2023 - Skiing at Lake Tahoe, NV", "2023/20230101/20230101c.html", "2023"],
+    ["2022/20221224/20221224.html", "December 24, 2022", "December 24, 2022 - Christmas at San Francisco, CA", "2022/20221224/20221224c.html", "2022"],
+    ["2022/20221204/20221204.html", "December 4, 2022", "December 4, 2022 - Christmas Tree at Home, WA", "2022/20221204/20221204c.html", "2022"],
+    ["2022/20221124/20221124.html", "November 24, 2022", "November 24, 2022 - Thanskgiving at Home, WA", "2022/20221124/20221124c.html", "2022"],
+     ];
 
 var middleRightTextPostList = "";
 var middleRightTextPostListDetail = "";
-for(var i = 0; i < PostList.length; i++) {
-    if (PostList[i][0] == PageName){
-        middleRightTextPostList += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][1] + "</a>" + 
-                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></b></div>";
-        middleRightTextPostListDetail += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][2] + "</a>" +
-                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></b></div>";
+var currentFolderName = "";
+
+for(var j = 0; j < FolderList.length; j++) {
+    currentFolderName = FolderList[j];
+    middleRightTextPostList += "<br/><div><b><a href='" + rootPath + "MyPosts/" + currentFolderName + "/index.html'>" + currentFolderName + "</a></b></div>";
+    middleRightTextPostListDetail += "<br/><div><b><a href='" + rootPath + "MyPosts/" + currentFolderName + "/index.html'>" + currentFolderName + "</a></b></div>";
+
+    for(var i = 0; i < PostList.length; i++) {
+        if ((PostList[i][4] == currentFolderName) && (currentFolderName == FolderName)) 
+        {
+            if (PostList[i][0] == PageName){
+                middleRightTextPostList += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][1] + "</a>" + 
+                                            "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></b></div>";
+                middleRightTextPostListDetail += "<div><b>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "'>" + PostList[i][2] + "</a>" +
+                                            "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></b></div>";
+            }
+            else {
+                middleRightTextPostList += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][1] + "</a>" +
+                                            "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></div>";
+                middleRightTextPostListDetail += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][2] + "</a>" +
+                                            "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></div>";
+            }
+        }
     }
-    else {
-        middleRightTextPostList += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][1] + "</a>" +
-                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></div>";
-        middleRightTextPostListDetail += "<div>   - <a href='" + rootPath + "MyPosts/" + PostList[i][0] + "' style='font-size: smaller;'>" + PostList[i][2] + "</a>" +
-                                    "  <a href='" + rootPath + "MyPosts/" + PostList[i][3] + "'> 中文 </a></div>";
-    }
-}
-        
+}       
 var middleRightTextPostDetails = "<br/><div><b><a href='" + rootPath + "MyPosts/index.html'>My Posts</a></b></div>" + middleRightTextPostList;
